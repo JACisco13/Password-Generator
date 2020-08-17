0d222d9f0fa6cc9a1991defd255e29e4da91e978
Joel C <xiiicisco@gmail.com>
2020-08-16 18:00:39 -0700

    generate function and characters

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Assignment code here
+var password = '';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numeric = '01234567890';
+var specialCharacter = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
+function generate() {
+  var confirmPasswordLength = '';
 
+  while (isNaN(confirmPasswordLength)) || confirmPasswordLength < 8 || confirmPasswordLength > 128) {confirmPasswordLength = prompt("What length would you like for this password? MUST BE 8 to 128 characters")}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
