006825bb30b3cf1949024b2811ff5e54ada2b475
Joel C <xiiicisco@gmail.com>
2020-08-16 19:53:57 -0700

    password criteria prompts

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 // Variables
+
+//Criteria Variables
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var number = '0123456789';
 var specialcharacter = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+
+//Criteria Selection Varriables
 var lowercaseSelection = false;
 var uppercaseSelection = false;
 var numberSelection = false;
 var specialcharacterSelection = false;
+
+//Password Variable
 var pswrd = '';
 
 //Generate Function
 function generate() {
     var confirmLength = '';
-//asking user to input desired character length
+
+    //User Character Length Input
     while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {confirmLength = prompt("How long would like your password? (MUST be between 8 to 128 characters)");
         if (confirmLength === null) {break;}
     }
 
+
+  //Password Criteria Prompts
+  if (confirmLength) {
+    if (confirm("Will password contain lowercase characters?") == true) {lowercaseSelection = true} 
+
+    if (confirm("Will password contain uppercase characters?") == true) {uppercaseSelection = true}
+
+    if (confirm("Will password contain numerical characters?") == true) {numberSelection = true}
+
+    if (confirm("Will password contain special characters?") == true) {specialcharacterSelection = true}
+    
+    //if none of the character types are selected, alerts the user to choose at least one
+    if (lowercaseSelection === false && uppercaseSelection === false && numberSelection === false && specialcharacterSelection === false) {alert("Password MUST contain at least 1 character type.")}
+  }
 }
 
 // Get references to the #generate element
